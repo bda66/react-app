541a6dd02b709d2f1cc2e1773c6d19bd59fd5de5
Dmitry Belyaev <dbelyaev@n-core.ru>
2017-10-05 18:55:01 +0300

    I'ts work

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,25 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+
+// import styles
+import './semantic/dist/components/button.css';
+import './semantic/dist/components/container.css';
+import './semantic/dist/components/divider.css';
+import './semantic/dist/components/flag.css';
+import './semantic/dist/components/header.css';
+import './semantic/dist/components/icon.css';
+import './semantic/dist/components/image.css';
+import './semantic/dist/components/input.css';
+import './semantic/dist/components/label.css';
+import './semantic/dist/components/list.css';
+import './semantic/dist/components/loader.css';
+import './semantic/dist/components/rail.css';
+import './semantic/dist/components/reveal.css';
+import './semantic/dist/components/segment.css';
+import './semantic/dist/components/step.css';
+import './semantic/dist/components/site.css';
+import './semantic/dist/components/reset.css';
 
 ReactDOM.render(
   <App />,
